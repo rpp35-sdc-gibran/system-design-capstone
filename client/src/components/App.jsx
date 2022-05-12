281d42ce702235625443a101beb8dd52e3126d2a
Shengnan Huang <selinahuang205@gmail.com>
2022-05-12 14:26:09 -0500

    before merging PR #4

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import ProductOverview from './product-overview-module/ProductOverview.jsx';
+<<<<<<< HEAD
 import axios from 'axios';
+=======
+import RatingsAndReviews from './ratings&reviews/RatingsAndReviews.jsx';
+>>>>>>> 191d1f1 (before merging PR #4)
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,9 +26,14 @@ class App extends Component {
     return (
       <div>
         Hello world
+<<<<<<< HEAD
         {this.state.currentProductId && (
           <ProductOverview currentProductId={this.state.currentProductId} />
         )}
+=======
+        <ProductOverview />
+        <RatingsAndReviews id={this.state.currentProductId}/>
+>>>>>>> 191d1f1 (before merging PR #4)
       </div>
     );
   }
