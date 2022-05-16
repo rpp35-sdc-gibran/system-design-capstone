b650c1dacae77eb2cdc8018e1794c94e0bd06d01
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-05-16 18:27:34 -0400

    Completed initial transform effect on image

--- client/src/components/product-overview-module/image-gallery/image-view-item/ImageViewItem.jsx
+++ client/src/components/product-overview-module/image-gallery/image-view-item/ImageViewItem.jsx
@@ -7,24 +7,63 @@ const ImageViewItem = ({
   handleChildZoom,
   isEnlargedView,
 }) => {
-  //handles conditionally changing className to switch views
+  const [backgroundPosition, setBackgroundPosition] = useState({});
+  const [isScaled, setIsScaled] = useState(false);
+  const [imagePosition, setImagePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  //handles conditionally changing className to toggle view
   const handleClick = () => {
     handleChildZoom();
   };
 
+  const handleTransformScaleView = () => {
+    console.log('transformed!!!');
+    setIsScaled(true);
+  };
+  const handleMouseMove = (e) => {
+    console.log('e.clientX:', e.clientX);
+    console.log('e.clienty:', e.clientY);
+    //change state to equal current mouse positoin
+    setImagePosition({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
+  const imageScaledContainerStyle = {
+    top: `${imagePosition.x}px`,
+    left: `${imagePosition.y}px`,
+  };
+
+  //enlarged image view
   if (isEnlargedView) {
     return (
       <>
-        <input type='checkbox'></input>
-        <div className='enlarged'>
-          <img src={image}></img>
-        </div>
+        <input
+          onMouseMove={handleMouseMove}
+          type='checkbox'
+          onClick={handleTransformScaleView}
+        ></input>
+        {/* scaled image view with inline style  */}
+        {isScaled ? (
+          <div style={imageScaledContainerStyle} className='enlarged'>
+            <img src={image}></img>
+          </div>
+        ) : (
+          <div className='enlarged'>
+            <img src={image}></img>
+          </div>
+        )}
       </>
     );
   } else {
+    //default image view
     return (
       <div className='default' onClick={handleClick}>
-        <img src={image}></img>
+        <img style={{ top: -50, left: -70 }} src={image}></img>
       </div>
     );
   }
