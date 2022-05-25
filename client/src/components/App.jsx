85a5d7ff69c680b8f68f3743a9e2d66d9a164ae4
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-05-25 14:11:02 -0400

    Begin refactor

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,42 +5,44 @@ import axios from 'axios';
 import RatingsAndReviews from './ratings&reviews/RatingsAndReviews.jsx';
 import sampleReviews from './ratings&reviews/reviews/reviewsForProductId71697.js';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      currentProductId: null,
-      currentReviews: sampleReviews.results,
-    };
-  }
-  componentDidMount() {
-    axios
-      .get('/api/products')
-      .then((products) => {
-        this.setState({
-          products: products.data,
-          currentProductId: products.data[0].id,
-        });
-      })
-      .catch((err) => {
-        console.log('err in App.jsx:', err);
-      });
-  }
-  render() {
-    return (
-      <div>
-        {this.state.currentProductId && (
-          <ProductOverview currentProductId={this.state.currentProductId} />
-        )}
-        {/* <QuestionsAnswers currentProductId={this.state.currentProductId} /> */}
-        {/* <RatingsAndReviews
+   constructor(props) {
+      super(props);
+      this.state = {
+         products: [],
+         currentProductId: null,
+         currentReviews: sampleReviews.results,
+      };
+   }
+   componentDidMount() {
+      axios
+         .get('/api/products')
+         .then((products) => {
+            this.setState({
+               products: products.data,
+               currentProductId: products.data[0].id,
+            });
+         })
+         .catch((err) => {
+            console.log('err in App.jsx:', err);
+         });
+   }
+   render() {
+      return (
+         <div>
+            {this.state.currentProductId && (
+               <ProductOverview
+                  currentProductId={this.state.currentProductId}
+               />
+            )}
+            {/* <QuestionsAnswers currentProductId={this.state.currentProductId} /> */}
+            {/* <RatingsAndReviews
           currentProductId={this.state.currentProductId}
           reviews={this.state.currentReviews}
           {...this.props}
         /> */}
-      </div>
-    );
-  }
+         </div>
+      );
+   }
 }
 
 export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './index.scss';
 import ReactDOM from 'react-dom/client';
 import App from './components/App.jsx';
-
+import { HashRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+//use hashrouter instead of browser router to not interfere with server
+root.render(
+   <HashRouter>
+      <Routes>
+         <Route path='/:id' element={<App />} />
+      </Routes>
+   </HashRouter>
+);
