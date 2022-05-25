82c4202b4dfd265f59e75d30b08fb14ed76a0865
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-05-24 20:42:24 -0400

    Completed aligning thumbnails

--- client/src/components/product-overview-module/image-gallery/image-view/ImageView.jsx
+++ client/src/components/product-overview-module/image-gallery/image-view/ImageView.jsx
@@ -10,93 +10,93 @@ import ImageViewThumbnails from '../image-view-thumbnails/ImageViewThumbnails.js
 import FitScreenIcon from '@mui/icons-material/FitScreen';
 
 const ImageView = ({
-  currentStylePhotos,
-  handleChildScale,
-  isScaled,
-  isEnlargedView,
-  setIsEnlargedView,
-  handleChildZoom,
+   currentStylePhotos,
+   handleChildScale,
+   isScaled,
+   isEnlargedView,
+   setIsEnlargedView,
+   handleChildZoom,
 }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+   const [currentIndex, setCurrentIndex] = useState(0);
 
-  //handles updating current slide on click next
-  const goNext = () => {
-    if (currentIndex < currentStylePhotos.length - 1) {
-      let newIndex = currentIndex;
-      setCurrentIndex(newIndex + 1);
-    }
-  };
+   //handles updating current slide on click next
+   const goNext = () => {
+      if (currentIndex < currentStylePhotos.length - 1) {
+         let newIndex = currentIndex;
+         setCurrentIndex(newIndex + 1);
+      }
+   };
 
-  //handles updating current slide on click previous
-  const goPrev = () => {
-    if (currentIndex > 0) {
-      let newIndex = currentIndex;
-      setCurrentIndex(newIndex - 1);
-    }
-  };
+   //handles updating current slide on click previous
+   const goPrev = () => {
+      if (currentIndex > 0) {
+         let newIndex = currentIndex;
+         setCurrentIndex(newIndex - 1);
+      }
+   };
 
-  //updates current index so that main image matches the clicked thumbnail
-  const handleThumbnailClick = (index) => {
-    setCurrentIndex(index);
-  };
+   //updates current index so that main image matches the clicked thumbnail
+   const handleThumbnailClick = (index) => {
+      setCurrentIndex(index);
+   };
 
-  return (
-    <div className='image-view'>
-      {!isScaled && (
-        <div className='image-view-thumbnails'>
-          <ImageViewThumbnails
-            photos={currentStylePhotos}
-            handleThumbnailClick={handleThumbnailClick}
-            currentIndex={currentIndex}
-            isEnlargedView={isEnlargedView}
-          />
-        </div>
-      )}
-      <div className='image-view-item'>
-        <ImageViewItem
-          image={currentStylePhotos[currentIndex].url}
-          handleChildZoom={handleChildZoom}
-          currentIndex={currentIndex}
-          isEnlargedView={isEnlargedView}
-          handleChildScale={handleChildScale}
-          isScaled={isScaled}
-        />
-      </div>
-      {!isScaled && (
-        <div className='image-view-buttons'>
-          {currentIndex !== 0 && (
-            <IconButton
-              className='button-prev'
-              data-testid='icon-prev'
-              onClick={goPrev}
-            >
-              <Avatar>
-                <ArrowBackIcon color='primary' fontSize='large' />
-              </Avatar>
-            </IconButton>
-          )}
-          {currentIndex !== currentStylePhotos.length - 1 && (
-            <IconButton
-              className='button-next'
-              data-testid='icon-next'
-              onClick={goNext}
-            >
-              <Avatar>
-                <ArrowForwardIcon color='primary' fontSize='large' />
-              </Avatar>
-            </IconButton>
-          )}
-          {/* <IconButton
+   return (
+      <div className='image-view'>
+         {!isScaled && (
+            <div className='image-view-thumbnails'>
+               <ImageViewThumbnails
+                  photos={currentStylePhotos}
+                  handleThumbnailClick={handleThumbnailClick}
+                  currentIndex={currentIndex}
+                  isEnlargedView={isEnlargedView}
+               />
+            </div>
+         )}
+         <div className='image-view-item'>
+            <ImageViewItem
+               image={currentStylePhotos[currentIndex].url}
+               handleChildZoom={handleChildZoom}
+               currentIndex={currentIndex}
+               isEnlargedView={isEnlargedView}
+               handleChildScale={handleChildScale}
+               isScaled={isScaled}
+            />
+         </div>
+         {!isScaled && (
+            <div className='image-view-buttons'>
+               {currentIndex !== 0 && (
+                  <IconButton
+                     className='button-prev'
+                     data-testid='icon-prev'
+                     onClick={goPrev}
+                  >
+                     <Avatar>
+                        <ArrowBackIcon color='primary' fontSize='large' />
+                     </Avatar>
+                  </IconButton>
+               )}
+               {currentIndex !== currentStylePhotos.length - 1 && (
+                  <IconButton
+                     className='button-next'
+                     data-testid='icon-next'
+                     onClick={goNext}
+                  >
+                     <Avatar>
+                        <ArrowForwardIcon color='primary' fontSize='large' />
+                     </Avatar>
+                  </IconButton>
+               )}
+               {/* <IconButton
             data-testid='enlarge-button'
             onClick={handleChildZoom}
             className='button-fit'
           >
             <FitScreenIcon />
           </IconButton> */}
-        </div>
-      )}
-    </div>
-  );
+            </div>
+         )}
+      </div>
+   );
 };
 
 export default ImageView;
