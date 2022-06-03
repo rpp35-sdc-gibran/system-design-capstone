c17d896c6ccb2d21dd8c6cdfb1f6680c1da1cb3f
Nourse41 <Nourse41@gmail.com>
2022-06-03 14:00:34 -0500

    first commit

--- client/src/components/questions-answers-module/add-answer/addAnswer.jsx
+++ client/src/components/questions-answers-module/add-answer/addAnswer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import './addQuestion.scss'
+import './addAnswer.scss'
 
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
